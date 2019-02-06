6f54e8231d0c3e7e4463271f80848634aa6dde4d
MatiasMoll <moll.matias.a@gmail.com>
2019-02-06 09:04:09 -0300

    Ejercicio 7, no funciona

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,32 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+	var numero1 = parseInt(numeroUno.value);
+	var numero2 = parseInt(numeroDos.value);
+	var resultado;
+
 function sumar()
 {	
-	
+	resultado = numero1 + numero2;
+	alert("La suma es " + resultado);
 }
 
 function restar()
 {
-	
+	resultado = numero1 - numero2;
+	alert("La resta es " + resultado);
 }
 
 function multiplicar()
 { 
-	
+	resultado = numero1 * numero2;
+	alert("La multiplicacion es " +resultado);
 }
 
 function dividir()
 {
-	
+	resultado = numero1 / numero2;
+	alert("La division es " +resultado);
 }
 
