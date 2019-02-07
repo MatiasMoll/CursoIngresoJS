ab42475ac8c1d2eca245bb94fa68c5ad258274f1
alumno <alumno>
2019-02-06 22:13:55 -0300

    Ejercicio 5 practica 2

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	var edadEntero;
+	var adolescente;
+
+	edad = document.getElementById('edad').value;
+	edadEntero = parseInt(edad);
+	adolescente = (edadEntero >= 13 && edadEntero <= 17);
+
+	if(!adolescente)
+	{
+		alert("No sos adolescente");
+	}
 
 
+
+		
 }//FIN DE LA FUNCIÓN
